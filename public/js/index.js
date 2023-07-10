865b4fa4c1e5f75b1b17f3f75d44d3bfbae1b341
maudnals <maud.nalpas@gmail.com>
2023-07-10 15:39:11 +0200

    Feat: Started UI migration

---
+++ public/js/index.js
@@ -0,0 +1,33 @@
+import { h, render, Component, Fragment } from 'preact'
+import { APP_VERSION, APP_NAME, MODES } from './config'
+import { Header } from './components/Header.jsx'
+// import { Main } from './components/Main.jsx'
+
+// Get the name of the currently selected mode
+// https:<BASE-URL>/?mode=simple
+function getCurrentModeFromUrl() {
+    const urlParams = new URLSearchParams(window.location.search)
+    return urlParams.get('mode')
+}
+
+// If the mode is unknown or URL scheme isn't standard, redirect to the simple mode as a fallback
+if (!Object.keys(MODES).includes(getCurrentModeFromUrl())) {
+    window.location.href = `${location.origin}/?mode=simple`
+}
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Header
+                    name={APP_NAME}
+                    version={APP_VERSION}
+                    currentMode={getCurrentModeFromUrl()}
+                ></Header>
+                {/* <Main currentMode={getCurrentModeFromUrl()}></Main> */}
+            </Fragment>
+        )
+    }
+}
+
+render(<App />, document.getElementById('app'))

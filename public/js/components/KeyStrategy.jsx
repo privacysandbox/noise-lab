56d1216cfb57b9dfeb3574c80f813a192db19484
maudnals <maud.nalpas@gmail.com>
2023-08-10 12:32:39 +0200

    Docs: Removed comment

--- public/js/components/KeyStrategy.jsx
+++ public/js/components/KeyStrategy.jsx
@@ -2,9 +2,6 @@ import { h, render, Component } from 'preact'
 
 export function KeyStrategy(props) {
     const { keyStrategy } = props
-
-    // TODO update budgetSplit when any of these change: (NONEED budgetSplitOption), numberOfMeasurementGoals, contributionBudget, noKeys
-
     return (
         <div>
             <label for="key-strategy">Key strategy:</label>

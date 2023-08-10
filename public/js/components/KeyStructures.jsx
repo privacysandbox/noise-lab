98553284aaa8637ee8500c64e63eb92e039ff268
maudnals <maud.nalpas@gmail.com>
2023-08-10 15:09:01 +0200

    Chore: Added OSS header

--- public/js/components/KeyStructures.jsx
+++ public/js/components/KeyStructures.jsx
@@ -1,3 +1,19 @@
+// TODO-CLEAN Delete this whole file
+
+/* Copyright 2022 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 import { h, render, Component } from 'preact'
 import { generateKeyStructure } from '../utils.misc'
 
@@ -60,89 +76,87 @@ export function KeyStructures(props) {
     )
 }
 
+// Key structure 1
+// x geography
+// o campaignId
+// x productCategory
 
+// Key structure 2
+// x geography
+// x campaignId
+// o productCategory
 
-    // Key structure 1
-    // x geography
-    // o campaignId
-    // x productCategory
+// Key structure 3
+// o geography
+// x campaignId
+// x productCategory
+// [
+//     {
+//         "names": [
+//             "geography",
+//             "productCategory"
+//         ],
+//         "combinations": [
+//             "3",
+//             "2"
+//         ]
+//     },
+//     {
+//         "names": [
+//             "geography",
+//             "campaignId"
+//         ],
+//         "combinations": [
+//             "3",
+//             "4"
+//         ]
+//     },
+//     {
+//         "names": [
+//             "campaignId",
+//             "productCategory"
+//         ],
+//         "combinations": [
+//             "4",
+//             "2"
+//         ]
+//     }
+// ]
 
-    // Key structure 2
-    // x geography
-    // x campaignId
-    // o productCategory
+// -----------------------------
+// -----------------------------
 
-    // Key structure 3
-    // o geography
-    // x campaignId
-    // x productCategory
-    // [
-    //     {
-    //         "names": [
-    //             "geography",
-    //             "productCategory"
-    //         ],
-    //         "combinations": [
-    //             "3",
-    //             "2"
-    //         ]
-    //     },
-    //     {
-    //         "names": [
-    //             "geography",
-    //             "campaignId"
-    //         ],
-    //         "combinations": [
-    //             "3",
-    //             "4"
-    //         ]
-    //     },
-    //     {
-    //         "names": [
-    //             "campaignId",
-    //             "productCategory"
-    //         ],
-    //         "combinations": [
-    //             "4",
-    //             "2"
-    //         ]
-    //     }
-    // ]
+// Key structure 1
+// o geography
+// x campaignId
+// x productCategory
 
-    // -----------------------------
-    // -----------------------------
+// Key structure 2
+// x geography
+// x campaignId
+// x productCategory
 
-    // Key structure 1
-    // o geography
-    // x campaignId
-    // x productCategory
-
-    // Key structure 2
-    // x geography
-    // x campaignId
-    // x productCategory
-
-    // [
-    //     {
-    //         "names": [
-    //             "campaignId",
-    //             "productCategory"
-    //         ],
-    //         "combinations": [
-    //             "4",
-    //             "2"
-    //         ]
-    //     },
-    //     {
-    //         "names": [
-    //             "geography",
-    //             "campaignId",
-    //             "productCategory"
-    //         ],
-    //         "combinations": [
-    //             "3",
-    //             "4",
-    //             "2"
-    //         ]
-    //     }
-    // ]
+// [
+//     {
+//         "names": [
+//             "campaignId",
+//             "productCategory"
+//         ],
+//         "combinations": [
+//             "4",
+//             "2"
+//         ]
+//     },
+//     {
+//         "names": [
+//             "geography",
+//             "campaignId",
+//             "productCategory"
+//         ],
+//         "combinations": [
+//             "3",
+//             "4",
+//             "2"
+//         ]
+//     }
+// ]

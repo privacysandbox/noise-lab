c496dc927ee81fd65d462b89468104a1e901c546
maudnals <maud.nalpas@gmail.com>
2023-08-10 15:02:08 +0200

    Fix: Added image

--- public/js/components/EmptyState.jsx
+++ public/js/components/EmptyState.jsx
@@ -1,11 +1,11 @@
 import { h, render, Component } from 'preact'
+import image from '../../assets/test-tube.png'
 
-// TODO-FIX-MINOR Image
 export function EmptyState(props) {
     return (
         <div id="empty-state">
             <div class="empty-state-inner">
-                <img src="../../assets/test-tube.png" alt="" />
+                <img src={image} />
                 <p>
                     No simulations yet. To generate your first simulation, click
                     the <strong>▶️ SIMULATE</strong> button at the bottom the{' '}
